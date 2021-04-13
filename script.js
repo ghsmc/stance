55373dc7c9e7d0a73dba1169934a9ffccf667566
George McCain <georgemccain88@gmail.com>
2021-04-13 10:30:10 -0400

    added formatting for dropdown

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+function search() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
